7baa63ec9fcd6305605fc9583cefe3f4e3e94634
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-08-11 00:56:14 +0700

    feat: modify Post Factory and seed database with 15 posts

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
         // Login
         Auth::login($user);
         // Redirect
-        return redirect()->route('home');
+        return view('posts.index');
     }
 
     // Login User
