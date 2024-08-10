2b0454f416c31134307e146d8b77f1f2d68a2df6
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-08-10 21:31:06 +0700

    feat: create AuthController with register method and update routing

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    // Register User
+    public function register(Request $request)
+    {
+        // Validate
+
+        // Register
+
+        // Login
+
+        // Redirect
+    }
+}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,28 @@
 <x-layout>
     <h1 class="title">Register a new account</h1>
+
+    <div class="mx-auto max-w-screen-sm card">
+        <form action="{{route('register')}}" method="post">
+            @csrf
+            <div class="mb-4">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" class="input">
+            </div>
+            <div class="mb-4">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" class="input">
+            </div>
+            <div class="mb-4">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="input">
+            </div>
+            <div class="mb-4">
+                <label for="password_confirmation">Confirm Password</label>
+                <input type="password" name="password_confirmation" id="password_confirmation" class="input">
+            </div>
+            <div class="mb-4">
+                <button type="submit" class="btn">Register</button>
+            </div>
+        </form>
+    </div>
 </x-layout>
