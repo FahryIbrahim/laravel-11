582ad220b050886ef37221d4efc822b5a03b03da
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-08-10 22:11:37 +0700

    feat: complete register method on AuthController and add login/guest indicators on dashboard

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,15 +14,15 @@ class AuthController extends Controller
         // Validate
         $data = $request->validate([
             'username' => ['required', 'max:255'],
-            'email' => ['required', 'max:255', 'email'],
+            'email' => ['required', 'max:255', 'email', 'unique:users'],
             'password' => ['required', 'confirmed', 'min:8'],
         ]);
 
-        dd('ok');
         // Register
-
+        $user = User::create($data);
         // Login
-
+        Auth::login($user);
         // Redirect
+        return redirect()->route('home');
     }
 }
