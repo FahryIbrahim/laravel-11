e249255bf21c3593c45b1b45ed4257a693a88256
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-08-10 21:50:42 +0700

    feat: add form validation and error handling in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,13 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         // Validate
+        $data = $request->validate([
+            'username' => ['required', 'max:255'],
+            'email' => ['required', 'max:255', 'email'],
+            'password' => ['required', 'confirmed', 'min:8'],
+        ]);
 
+        dd('ok');
         // Register
 
         // Login
