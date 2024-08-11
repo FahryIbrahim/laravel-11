9a8970be23d8fc257f53a3229d2d19af9958fa93
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-08-11 09:48:45 +0700

    feat: send email every create new post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UpdatePostRequest;
+use App\Mail\WelcomeMail;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller implements HasMiddleware
@@ -24,6 +26,7 @@ class PostController extends Controller implements HasMiddleware
      */
     public function index()
     {
+
         $posts = Post::latest()->paginate(6);
 
         return view('posts.index', ['posts' => $posts]);
@@ -54,11 +57,13 @@ class PostController extends Controller implements HasMiddleware
             $path = Storage::disk('public')->put('posts_images', $request->image);
         }
         // Create a post
-        Auth::user()->posts()->create([
+        $post = Auth::user()->posts()->create([
             'title' => $request->title,
             'body' => $request->body,
             'image' => $path
         ]);
+        // Send email
+        Mail::to(Auth::user())->send(new WelcomeMail(Auth::user(), $post));
         // Redirect to the dashboard
         return back()->with('success', 'Your post was created');
     }
